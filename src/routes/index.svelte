07b2e65907674e4b5214d98a23fcf534e342dbc8
IntricEight <cjbowdoin@gmail.com>
2021-09-21 16:37:12 -0700

    Added dice counter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let numDice = 2;
+</script>
+
+<input type="number" bind:value={numDice} min="1" max="100" />
+
+<p>{numDice}</p>
+
+<div style="display: flex;">
+	{#each { length: numDice } as die, index}
+		<div class="dice">
+			{index + 1}
+		</div>
+	{/each}
+</div>
+
+<style>
+	.dice {
+		padding: 5px;
+		margin: 5px;
+		background: lightgray;
+	}
+</style>
